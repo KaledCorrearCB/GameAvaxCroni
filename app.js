29013a9abd67098a7b987b8ee85ee18ef67e584d
KaledCorrearCB <56941264+KaledCorrearCB@users.noreply.github.com>
2024-09-01 22:29:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const { Web3 } = require("web3");
+const contractInfo = require("./constants.json");
+
+const address = "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC";
+const privateKey =
+  "0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027";
+const rpc =
+  "https://laughing-umbrella-j7qv76qxq66fqrgg-9650.app.github.dev/ext/bc/crono/rpc";
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hola Bogotá!");
+});
+
+app.get("/contract-info", async (req, res) => {
+  const lucasNet = new Web3(rpc);
+  const balanceLucas = await lucasNet.eth.getBalance(address);
+  console.log({ balanceLucas });
+  res.send({
+    message: "Hola Bogotá...!",
+    balanceLucas: Number(balanceLucas) / 10 ** 18,
+  });
+});
+
+app.post("/transfer-native-token", async (req, res) => {
+  const lucasNet = new Web3(rpc);
+  const account = lucasNet.eth.accounts.wallet.add(privateKey);
+  const tx = {
+    from: account[0].address,
+    to: req.body.receiver,
+    value: lucasNet.utils.toWei(String(req.body.amount), "ether"),
+  };
+  const txReceipt = await lucasNet.eth.sendTransaction(tx);
+  console.log("Tx hash:", txReceipt.transactionHash);
+  console.log(txReceipt.transactionHash);
+  res.send({
+    message: "Ok",
+    statusCode: 200,
+    txHash: txReceipt.transactionHash,
+  });
+});
+
+app.post("/tranfer-diamond", async (req, res) => {
+  const data = req.body;
+  const lucasNet = new Web3(rpc);
+  const account = lucasNet.eth.accounts.wallet.add(privateKey);
+  let diamonds = new lucasNet.eth.Contract(
+    contractInfo.abi,
+    contractInfo.address
+  );
+  const transferDiamond = await diamonds.methods
+    .transfer(data.receiver, data.amount)
+    .send({
+      from: account[0].address,
+    });
+
+  res.send({
+    message: "Ok",
+    statusCode: 200,
+    txHash: transferDiamond.transactionHash,
+  });
+});
+
+app.post("/transfer", async (req, res) => {
+  const data = req.body;
+  const lucasNet = new Web3(rpc);
+  const account = lucasNet.eth.accounts.wallet.add(privateKey);
+  let diamonds = new lucasNet.eth.Contract(
+    contractInfo.abi,
+    contractInfo.address
+  );
+  const tx = {
+    from: account[0].address,
+    to: data.receiver,
+    value: data.emeralds,
+  };
+  //   const gasEmeralds = await lucasNet.eth.estimateGas(tx);
+  const txReceipt = await lucasNet.eth.sendTransaction(tx);
+  const transferDiamond = await diamonds.methods
+    .transfer(data.receiver, data.diamonds)
+    .send({
+      from: account[0].address,
+    });
+  //   const gasDiamond = await diamonds.methods
+  //     .transfer(data.receiver, data.diamonds)
+  //     .estimateGas();
+  res.send({
+    message: "Ok",
+    statusCode: 200,
+    txHashDiamond: transferDiamond.transactionHash,
+    txHashEmeralds: txReceipt.transactionHash,
+    // gasDiamond: Number(gasDiamond),
+    // gasEmeralds: Number(gasEmeralds),
+  });
+});
+
+//0.9465
+// 0.000...1
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
